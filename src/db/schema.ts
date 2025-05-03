fd4bdf267efefc5f507a93dbdc891660f5ea85bd
João Victor Gadelha <joaovictor_gadelha@live.com>
2025-05-03 15:22:20 -0300

    Add initial project setup with environment configuration, database schema, and Docker support

---
+++ src/db/schema.ts
@@ -0,0 +1,66 @@
+import {
+  pgTable,
+  uuid,
+  text,
+  integer,
+  timestamp,
+  pgEnum,
+} from "drizzle-orm/pg-core";
+
+// USERS table
+export const role = pgEnum("role", ["ADMIN", "LIBRARIAN", "USER"]);
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(), // Usando UUID como chave primária com geração automática
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  passwordHash: text("password_hash").notNull(),
+  role: role("role").default("USER").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+// BOOKS table
+export const books = pgTable("books", {
+  id: uuid("id").primaryKey().defaultRandom(), // Usando UUID como chave primária com geração automática
+  title: text("title").notNull(),
+  author: text("author").notNull(),
+  language: text("language").notNull(),
+  isbn: text("isbn").unique(),
+  description: text("description"),
+  copiesAvailable: integer("copies_available").notNull(),
+  totalCopies: integer("total_copies").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+// LOANS table
+export const status = pgEnum("status", [
+  "PENDING",
+  "IN_PROGRESS",
+  "RETURNED",
+  "LATE",
+]);
+export const loans = pgTable("loans", {
+  id: uuid("id").primaryKey().defaultRandom(), // Usando UUID como chave primária com geração automática
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id),
+  bookId: uuid("book_id")
+    .notNull()
+    .references(() => books.id),
+  status: status("status").default("PENDING").notNull(),
+  loanDate: timestamp("loan_date"),
+  dueDate: timestamp("due_date"),
+  returnDate: timestamp("return_date"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+// RESERVATIONS table
+export const reservations = pgTable("reservations", {
+  id: uuid("id").primaryKey().defaultRandom(), // Usando UUID como chave primária com geração automática
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id),
+  bookId: uuid("book_id")
+    .notNull()
+    .references(() => books.id),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
